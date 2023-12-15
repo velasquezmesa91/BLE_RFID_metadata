ee3223d103035b6365cb6f1700c4477a171bbf3e
Francisco <frankj.mortiz@gmail.com>
2023-12-15 12:22:40 -0500

    revision de mas tamaños

--- BLE_RFID_check.py
+++ BLE_RFID_check.py
@@ -4,9 +4,27 @@ from streamlit import session_state as ss
 import numpy as np
 
 st.title("Chequeos formulario metadatos" )
+tamaño_ind = st.selectbox("Seleccione el tamaño",
+                              ["60x40x13","60x40x18","60x40x25",
+                               "60x40x24","30x40x15","30x40x21","60x40x21"])
 tab1, tab2, tab3 = st.tabs(["Chequeo Individual","Chequeo Pares","Solo RFID"])
+
 bad_serials = pd.read_excel("bad_serials.xlsx")
+dict_tamaños ={
+    "60x40x13":280,
+    "60x40x25":210,
+    "30x40x15":560,
+    "60x40x18":0,
+    "60x40x24":0,
+    "30x40x21":0,
+    "60x40x21":0,
+}
+
 with tab1:
+
+    
+    tam1 = dict_tamaños[tamaño_ind]
+  
     arch1 = st.file_uploader("Cargue el archivo a revisar")
     if arch1:
         df1 = pd.read_excel(arch1, skiprows=4)
@@ -22,12 +40,12 @@ with tab1:
         BLE_long = ble["Device ID"].str.len().unique()
         RFID_long = rfid["Device ID"].str.len().unique()
         errores=0
-        if BLE_unico !=210:
+        if BLE_unico != tam1:
             errores+=1
-            st.error(f"Hay {BLE_unico} BLE registrados y deberian ser 210, revise el archivo")
-        if RFID_unico != 210:
+            st.error(f"Hay {BLE_unico} BLE registrados y deberian ser {tam1}, revise el archivo")
+        if RFID_unico != tam1:
             errores+=1
-            st.error(f"Hay {RFID_unico} RFID registrados y deberian ser 210, revise el archivo")
+            st.error(f"Hay {RFID_unico} RFID registrados y deberian ser {tam1}, revise el archivo")
         if any(BLE_duplicado):
             errores+=1
             st.error("Hay dispositivos BLE duplicados, revise el archivo")
@@ -85,7 +103,7 @@ with tab2:
 
         df_join = df12.merge(df22, on="key", how="inner",suffixes=["","_R"])
 
-        if tm[0]!=420:
+        if tm[0]!=(tam1*2):
             st.error("Los archivos tienen diferentes seriales, revise los archivos")
 
             st.write(f"RFID en archivo 1 y no en 2: {rfid1[~rfid1.isin(rfid2)].unique()}")
@@ -93,7 +111,7 @@ with tab2:
             st.write(f"BLE en archivo 1 y no en 2: {ble1[~ble1.isin(ble2)].unique()}")
             st.write(f"BLE en archivo 2 y no en 1: {ble2[~ble2.isin(ble1)].unique()}")
 
-        elif df_join.shape[0] !=210:
+        elif df_join.shape[0] !=tam1:
             st.error("Los archivos tienen diferentes seriales, revise los archivos")
             st.write(f"RFID en archivo 1 y no en 2: {rfid1[~rfid1.isin(rfid2)].values}")
             st.write(f"RFID en archivo 2 y no en 1: {rfid2[~rfid2.isin(rfid1)].values}")
